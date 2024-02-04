73f35ff88f4160ad55e2632ffeb8f7593d11e016
Erfan <eftekharierfan5555555555@gmail.com>
2024-02-04 13:41:17 +0330

    finish project routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./Input.css"
-import ProMusicPlayer from "./components/ProMusicPlayer/ProMusicPlayer"
+import Main from "./components/Main/Main"
+import Player from "./components/Player/Player"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
     return (
-        <div>
-            <ProMusicPlayer />
-        </div>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/player" element={<Player />} />
+        </Routes>
     )
 }
 
