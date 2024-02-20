6c6ba0074c15ece2e6e8662e8f2721351f6c09df
Erfan <eftekharierfan5555555555@gmail.com>
2024-02-20 22:50:07 +0330

    change project title and favicon

--- src/components/Pages/Account.jsx
+++ src/components/Pages/Account.jsx
@@ -1,12 +1,13 @@
 import { Navigate } from 'react-router-dom'
-import { isLogin } from '../../utils'
-import { StateDispatcher } from '../ReducerAndContexts/ReducerAndContexts'
+import { getUserInfo, isLogin } from '../../utils'
+import { StateDispatcher, mainUserData } from '../ReducerAndContexts/ReducerAndContexts'
 import { useContext, useState } from 'react'
 
 export default function Account() {
 
     const dispatch = useContext(StateDispatcher)
     const [update, setUpdate] = useState(false)
+    console.log(getUserInfo().user.email);
 
     const logoutHandler = () => {
         localStorage.removeItem("sb-inbskwhewximhtmsxqxi-auth-token")
@@ -17,7 +18,13 @@ export default function Account() {
 
     return (
         <>
-            {!isLogin() ? <Navigate replace={true} to="/login" /> : <button onClick={logoutHandler} className="bg-primaryOrange py-2 ml-auto m-4 block w-24 font-bold rounded-md cursor-pointer">Log out</button>}
+            {!isLogin() ? <Navigate replace={true} to="/login" /> : 
+                <div className='container ch:break-words mt-4'>
+                    <div className='neoM-bg py-2 px-3 mt-3'>Username: {getUserInfo().user.user_metadata.username}</div>
+                    <div className='neoM-bg py-2 px-3 mt-3'>Email: {getUserInfo().user.email}</div>
+                    <button onClick={logoutHandler} className="bg-primaryOrange mt-12 py-2 block neoM-buttons w-24 font-bold rounded-md cursor-pointer">Log out</button>
+                </div>
+            }
         </>
     )
 }
