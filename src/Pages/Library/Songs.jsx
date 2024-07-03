c42c09ec348b544f19f9261cfaed3f837f1a61d8
Erfan <eftekharierfan5555555555@gmail.com>
2024-07-03 19:03:23 +0330

    refactor Library components

--- src/Pages/Library/Songs.jsx
+++ src/Pages/Library/Songs.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { IoArrowBack } from "react-icons/io5";
 import { LiaRandomSolid } from "react-icons/lia";
@@ -11,68 +11,78 @@ import { getUserInfo, isLogin } from '../../utils';
 
 export default function Songs() {
 
+    const { isShuffle, isPlaying, filteredSongsUpdater } = useContext(States)
+    const dispatch = useContext(StateDispatcher)
     const [mainUserData, setMainUserData] = useState([])
     const [filteredSongs, setFilteredSongs] = useState([])
     const [search, setSearch] = useState("")
-    const [update, setUpdate] = useState(false)
-    const { isShuffle, isPlaying, filteredSongsUpdater, stopMusic } = useContext(States)
-    const dispatch = useContext(StateDispatcher)
 
     const navigate = useNavigate()
     const params = useParams()
-    const audio = useRef()
-    
-    useEffect(() => {isLogin() && setMainUserData(getUserInfo().user.user_metadata.songs)}, [update, filteredSongsUpdater]);
 
     useEffect(() => {
+        isLogin() && setMainUserData(getUserInfo().user.user_metadata.songs)
+    }, [filteredSongsUpdater]);
+
+    useEffect(() => {
+
         if (mainUserData.length) {
+
             let filteredSongs;
+
             switch (params.type) {
-                case "recentlies":
-                    filteredSongs = [...mainUserData].reverse();
-                    break;
-                case "playlist":
-                    filteredSongs = mainUserData.filter(song => song.favorite);
-                    break;
-                case "favorites":
-                    filteredSongs = mainUserData.filter(song => song.liked);
-                    break;
-                case "/":
-                    filteredSongs = mainUserData.filter(song => song.liked);
-                    break;
-                default:
-                    filteredSongs = mainUserData;
-                    break;
+                case "recentlies": { filteredSongs = [...mainUserData].reverse(); break }
+                case "playlist": { filteredSongs = mainUserData.filter(song => song.favorite); break }
+                case "favorites": { filteredSongs = mainUserData.filter(song => song.liked); break }
+                case "/": { filteredSongs = mainUserData.filter(song => song.liked); break }
+                default: filteredSongs = mainUserData;
             }
+
             setFilteredSongs(filteredSongs);
         }
     }, [mainUserData, params.type]);
 
     const songSearchHandler = e => {
+        const newSongsFilter = [...mainUserData].filter(song => song.name.toLowerCase().includes(e.target.value.toLowerCase()))
         setSearch(e.target.value)
-        let newSongsFilter = [...mainUserData].filter(song => song.name.toLowerCase().includes(e.target.value.toLowerCase()))
         setFilteredSongs(newSongsFilter)
     }
 
     return (
         <>
             <section className='container'>
+
                 <div className='flex items-center gap-6 my-6'>
                     <div onClick={() => navigate(-1)} className='flex cursor-pointer items-center justify-center bg-primary neoM-buttons duration-200 transition-all rounded-full ch:size-8 p-2'><IoArrowBack /></div>
                     <h3 className='font-bold text-2xl capitalize'>{params.type || "All songs"}</h3>
                 </div>
 
                 <div className='flex items-center gap-2 justify-between px-3 basis-[85%] h-12 neoM-bg ' >
-                    <input value={search} onChange={songSearchHandler} className='flex-[6] bg-primary outline-none placeholder:text-red-400/65 text-red-400/65' placeholder={`Search in ${filteredSongs?.length} musics...`} type="text" />
+                    <input
+                        value={search}
+                        onChange={songSearchHandler}
+                        className='flex-[6] bg-primary outline-none placeholder:text-red-400/65 text-red-400/65'
+                        placeholder={`Search in ${filteredSongs?.length} musics...`}
+                        type="text"
+                    />
                     <div className='cursor-pointer'><CiSearch className='size-6 flex-1' /></div>
                 </div>
+
                 <div className='flex items-center justify-end gap-4 ch:cursor-pointer my-6'>
 
-                    <div onClick={() => { dispatch({ type: "changeShuffle", payload: !isShuffle }), dispatch({ type: "shouldRepeatChanger", payload: isShuffle && false }) }} className={`flex items-center justify-center bg-primary neoM-buttons duration-200 transition-all ${isShuffle && "text-primaryOrange"} duration-200 rounded-full ch:size-7 p-2`}><LiaRandomSolid /></div>
+                    <div onClick={() => {
+                        dispatch({ type: "changeShuffle", payload: !isShuffle })
+                        dispatch({ type: "shouldRepeatChanger", payload: isShuffle && false })
+                    }}
+                        className={`flex items-center justify-center bg-primary neoM-buttons duration-200 transition-all ${isShuffle && "text-primaryOrange"} duration-200 rounded-full ch:size-7 p-2`}
+                    >
+                        <LiaRandomSolid />
+                    </div>
 
                     <div
                         onClick={() => dispatch({ type: isPlaying ? "pause" : "play" })}
-                        className='flex items-center justify-center text-primary bg-primaryWhite rounded-full ch:size-7 p-2'>
+                        className='flex items-center justify-center text-primary bg-primaryWhite rounded-full ch:size-7 p-2'
+                    >
                         {
                             isPlaying ? <GiPauseButton /> : <VscDebugStart />
                         }
@@ -81,12 +91,14 @@ export default function Songs() {
 
                 <div className='space-y-2'>
                     {
-                        !filteredSongs.length ?
+                        !filteredSongs.length
+                            ?
                             <div className='text-2xl font-bold text-center'>No songs yet...</div>
                             :
                             filteredSongs.map(song => <Track key={song.id} {...song} />)
                     }
                 </div>
+
             </section>
         </>
     )
--- src/Pages/Library/Track.jsx
+++ src/Pages/Library/Track.jsx
@@ -14,12 +14,13 @@ import { getUserInfo, tagRemover } from '../../utils';
 
 export default function Track(data) {
 
-    const [showDetails, setShowDetails] = useState(false)
-    const { userSongsStorage, userData, currentSong, isPlaying, like, share, songIndex } = useContext(States)
-    const [loader, setLoader] = useState(false)
     const dispatch = useContext(StateDispatcher)
+    const [showDetails, setShowDetails] = useState(false)
+    const [loader, setLoader] = useState(false)
+    const { userSongsStorage, userData, currentSong, isPlaying, like, share, songIndex } = useContext(States)
     const { cover, id, name, artistname, favorite } = data
-    const palyerHandler = () => { dispatch({ type: "changeCurrent", payload: [...userSongsStorage].findIndex(song => song.name == name) }) }
+
+    const playerHandler = () => { dispatch({ type: "changeCurrent", payload: [...userSongsStorage].findIndex(song => song.name == name) }) }
 
     const songLikeToggler = async () => {
         await like("favorite", name)
@@ -27,33 +28,34 @@ export default function Track(data) {
     }
 
     const deleteHandler = async () => {
+
         const updatedData = [...mainUserData.songs].filter(song => song.id !== id)
         setLoader(true)
-        // make it separate later
+
         try {
-            const { data, error } = await supabase.storage.from("users").remove(userData[0].user.email + `/${name}`)
+
+            const { error } = await supabase.storage.from("users").remove(userData[0].user.email + `/${name}`)
             if (error) throw new Error(error)
 
-            const { updatedSongsData, updatedSongsError } = await supabase.auth.updateUser({ data: { songs: updatedData } })
+            const { updatedSongsError } = await supabase.auth.updateUser({ data: { songs: updatedData } })
             if (updatedSongsError) throw new Error(updatedSongsError)
 
             // check if songs array gets empty
-            let arrayAfterRemove = getUserInfo().user.user_metadata.songs.filter(song => song.name !== name)
+            const arrayAfterRemove = getUserInfo().user.user_metadata.songs.filter(song => song.name !== name)
+
             if (currentSong && currentSong.name == name) dispatch({ type: "changeCurrent", payload: songIndex == mainUserData?.songs.length - 1 ? 0 : songIndex + 1 })
-            if (!arrayAfterRemove.length) {
-                location.reload()
-                return
-            }
+            if (!arrayAfterRemove.length) return location.reload()
 
             dispatch({ type: "filteredSongsUpdater" })
             dispatch({ type: "updater" })
-            
+
             dispatch({
                 type: "toastOn",
                 text: "Music deleted successfully !",
                 status: 1
             })
             setTimeout(() => dispatch({ type: "toastOff" }), 3000);
+
         } catch (error) {
             console.log(error);
             dispatch({
@@ -63,14 +65,19 @@ export default function Track(data) {
             })
             setTimeout(() => dispatch({ type: "toastOff" }), 2000);
         }
-        setShowDetails(false)
-        setLoader(false)
+        finally {
+            setShowDetails(false)
+            setLoader(false)
+        }
     }
 
     const musicDownloadHandler = async () => {
+
         try {
+
             const { data, error } = await supabase.storage.from("users").download(getUserInfo().user.email + "/" + name);
             if (error) throw new Error(error)
+
             downloadFile(data, name);
 
             dispatch({
@@ -79,6 +86,7 @@ export default function Track(data) {
                 status: 1
             })
             setTimeout(() => dispatch({ type: "toastOff" }), 2000);
+
         } catch (error) {
             dispatch({
                 type: "toastOn",
@@ -87,12 +95,14 @@ export default function Track(data) {
             })
             setTimeout(() => dispatch({ type: "toastOff" }), 2000);
         }
-        setShowDetails(false)
+        finally { setShowDetails(false) }
     };
 
     const downloadFile = (musicBlob, fileName) => {
+
         const blobUrl = URL.createObjectURL(musicBlob);
         const link = document.createElement('a');
+
         link.href = blobUrl;
         link.download = fileName
         document.body.appendChild(link);
@@ -102,8 +112,11 @@ export default function Track(data) {
     };
 
     return (
+
         <div className='flex items-center gap-3'>
+
             <div onClick={() => setShowDetails(false)} className={` ${showDetails ? "z-40 visible" : " invisible z-0"} fixed z-30 inset-0`}></div>
+
             {
                 cover ?
                     <img className='flex-1 shrink-0 size-14 rounded-sm object-cover aspect-square' src={cover} alt="img" />
@@ -111,36 +124,55 @@ export default function Track(data) {
                     <div className='flex duration-300'><FiMusic className='size-[34px] p-1' /></div>
             }
 
-            <div onClick={palyerHandler} className='flex-[7] text-sm text-gray-200/50 cursor-pointer'>
+            <div onClick={playerHandler} className='flex-[7] text-sm text-gray-200/50 cursor-pointer'>
+
                 <h3 className='font-bold text-md text-primaryWhite line-clamp-1 max-w-[210px] ]'>{tagRemover(name) || "?"}</h3>
+
                 <p className='text-sm'>{artistname || "?"}</p>
+
                 <div className='flex gap-2 items-center text-xs'>
+
                     {
                         (currentSong?.name == name && isPlaying) ?
                             <p className='flex items-center text-red-400'>Now Playing</p>
                             :
                             <IoTriangle className='rotate-90 size-[10px]' />
                     }
-
                     <p>{getUserInfo().user.user_metadata.songs.find(song => song.name == name)?.duration}</p>
+
                 </div>
+
             </div>
+
             <div onClick={() => setShowDetails(true)} className='flex-[1/2] active:bg-black/25 duration-200 relative aspect-square rounded-full'>
+
                 <PiDotsThreeVerticalBold className='size-[34px] p-1 z-0 cursor-pointer' />
+
                 <div className={`inset-0 -left-[185px] z-40 ${showDetails ? "absolute" : "hidden"} neoM-bg h-36 p-4 -my-4 z-0`}>
+
                     <ul className='space-y-2 ch:flex ch:items-center ch:gap-2 ch:justify-between ch:cursor-pointer ch:duration-200 ch:opacity-70 ch-hover:opacity-100'>
+
                         <li onClick={songLikeToggler}>{favorite ? "Remove from" : "Add to"} playlist <MdPlaylistAdd className='shrink-0' /></li>
+
                         <li onClick={musicDownloadHandler}>Download <FaDownload /> </li>
+
                         <li onClick={() => share(musicUrl)}>Share <IoMdShare /></li>
-                        <li className='text-primaryOrange ' onClick={deleteHandler}>Delete {
-                            !loader ? <MdDelete /> : <Oval
-                                visible={true}
-                                height="20"
-                                width="20"
-                                color="#A64717"
-                                ariaLabel="oval-loading"
-                            />
-                        }</li>
+
+                        <li
+                            className='text-primaryOrange'
+                            onClick={deleteHandler}
+                        >
+                            Delete
+                            {
+                                !loader ? <MdDelete /> : <Oval
+                                    visible={true}
+                                    height="20"
+                                    width="20"
+                                    color="#A64717"
+                                    ariaLabel="oval-loading"
+                                />
+                            }
+                        </li>
                     </ul>
                 </div>
             </div>
